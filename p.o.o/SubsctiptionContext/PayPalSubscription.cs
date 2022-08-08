a17e3d44d3d9c061b8bc9d5ae9e7940a2492fee3
Luiz Henrique Lajús <95000306+LuizHenriqueLajus@users.noreply.github.com>
2022-08-08 01:08:03 -0300

    Ajustes finais.

--- p.o.o/SubsctiptionContext/PayPalSubscription.cs
+++ p.o.o/SubsctiptionContext/PayPalSubscription.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace p.o.o.SubsctiptionContext
+﻿namespace p.o.o.SubsctiptionContext
 {
-    internal class PayPalSubscription
+    public class PayPalSubscription : Subscription
     {
     }
 }
